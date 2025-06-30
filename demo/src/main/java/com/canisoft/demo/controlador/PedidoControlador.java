1342f14a98dbd5a620e601c905878c578b4900c4
nicodiaz1223 <nicodiaz1223@gmail.com>
2025-06-29 19:13:20 -0500

    CROS arreglado

--- demo/src/main/java/com/canisoft/demo/controlador/PedidoControlador.java
+++ demo/src/main/java/com/canisoft/demo/controlador/PedidoControlador.java
@@ -11,6 +11,7 @@ import java.util.List;
 /**
  * Controlador REST para gestionar pedidos.
  */
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/pedidos")
 public class PedidoControlador {
